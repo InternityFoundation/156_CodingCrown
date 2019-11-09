e8a4f9066552a416eec3d352f937c1c0cdc39204
Lakshay Singhal <47491990+lakshay05singhal@users.noreply.github.com>
2019-11-10 00:26:51 +0530

    first final commit
    organized the files and directory for SVN

---
+++ app/src/main/java/com/example/healthcare/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // for new activity page for health care tab
+    public void healthCare(View view){
+        Intent intent1 = new Intent(this, HealthActivity.class);
+        startActivity(intent1);
+    }
+
+    // for new activity page for emergency tab
+    public void emergency(View view){
+        Intent intent2 = new Intent(this, EmergencyActivity.class);
+        startActivity(intent2);
+    }
+}
